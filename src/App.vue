ccf1174c47d65dff3c278b20504389cb149fe850
Buuchezo <buuchezoh@hotmail.co.uk>
2025-06-02 14:23:58 +0200

    fiftinth version

--- src/App.vue
+++ src/App.vue
@@ -129,7 +129,7 @@ export default defineComponent({
 
         return
       }
-      const song = `${enteredKunstler.value}-${enteredTitel.value}`
+      const song = `${enteredKunstler.value} - ${enteredTitel.value}`
       try {
         await fetch('https://music-list-cadfa-default-rtdb.firebaseio.com/songs.json', {
           method: 'POST',
